ae28d4f83acdad2acccc47b10167034789d09f75
Nev <87309932+nevanII@users.noreply.github.com>
2022-03-17 10:04:27 +0000

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Console
+
+
+Module Module1
+
+    Sub Main()
+        PellNumbers()
+
+
+        ReadKey()
+
+    End Sub
+    Sub PellNumbers()
+
+        Dim n1, n2, n3, ninput As Integer
+        n1 = 0
+        n2 = 1
+        n3 = (n2 * 2) + n1
+
+
+
+
+        WriteLine("What is the number that you would like to loop to?")
+        ninput = ReadLine()
+
+        WriteLine("The Pell numbers up to {0} are :", ninput)
+        Write(n1)
+        Write(",")
+        For i = 1 To ninput
+
+            n1 = n2
+            Write(n1)
+            Write(",")
+
+            n2 = n3
+
+
+            n3 = (n2 * 2) + n1
+        Next
+
+        Write(n2)
+        Write(",")
+        Write(n3)
+
+        ReadKey()
+    End Sub
+
+End Module
